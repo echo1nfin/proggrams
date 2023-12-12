527ce4e5061e6823428d706a65d45cba090ed1e4
echo1 <coloveyx@gamil.com>
2023-12-12 12:06:02 +0500

    test python cycles

---
+++ cycles.py
@@ -0,0 +1,12 @@
+from time import time
+
+start = time()
+for i in range(10_000_000):
+    pass
+
+print(time() - start)
+n = 0
+start = time()
+while n < 10_000_000:
+    n += 1
+print(time() - start)
