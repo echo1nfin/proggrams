d65bc30bec869deccc6aabc3b69201ee47e70124
echo1 <coloveyx@gamil.com>
2023-12-12 12:02:40 +0500

    new ex. on python

--- stepik.py
+++
@@ -1,8 +0,0 @@
-import re
-
-n = int(input())
-strings = [input() for _ in range(n)]
-
-for i in range(len(strings)):
-    if re.findall(r'.*a.*n.*t.*o.*n', strings[i]):
-        print(i+1, end = ' ')
